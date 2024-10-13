82bb476e7db11f1ecb94662148f72e26bdf1d0cd
FilippSM_laptop <philippsm666@gmail.com>
2024-10-13 22:38:42 +0300

    add Main

---
+++ src/components/FlexWrapper.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+type FlexWrapperPropsType = {
+    direction?: string
+    justify?: string
+    align?: string
+    wrap?: string
+}
+
+/* универсальный компонент в который будут передавать свойства для флексов (исп. как обертка)
+и применял для тех элементов которые будут выравниваться */
+
+export const FlexWrapper = styled.div<FlexWrapperPropsType>`
+    display: flex;
+    flex-direction: ${props => props.direction || "row"}; /* пропс или значение по умолчанию   */
+    justify-content: ${props => props.justify || "flex-start"};
+    align-items: ${props => props.align || "stretch"};
+    flex-wrap: ${props => props.wrap || "nowrap"}
+`
---
+++ src/layout/section/Main.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import photo from "./../../assets/images/Photo.jpg"
+import { FlexWrapper } from "../../components/FlexWrapper";
+
+export const Main = () => {
+    return (
+        <div>
+            <StyledMain>
+                <FlexWrapper align={"center"} justify={"space-around"}>
+                    <div>
+                        <span>Hi There</span>
+                        <Name>I'm Filipp Samsonau</Name>
+                        <MainTitle>A web Developer.</MainTitle>
+                    </div>
+                    <Photo src={photo} alt="" />
+                </FlexWrapper>
+            </StyledMain>
+        </div>
+    );
+};
+
+const StyledMain = styled.div`
+    min-height: 100vh;
+    background-color: #c6b6b6;
+`
+
+const Photo = styled.img`
+    width: 350px;
+    height: 430px;
+    object-fit: cover;
+`
+
+const MainTitle = styled.h1`
+    
+`
+const Name = styled.h2`
+    
+`
