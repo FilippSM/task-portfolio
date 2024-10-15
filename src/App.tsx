21850f67c7ccd00dc9c01077d25086f0653b87eb
FilippSM_laptop <philippsm666@gmail.com>
2024-10-15 23:28:59 +0300

    add Contacts

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Main } from './layout/section/main/Main';
 import { Skills } from './layout/section/skills/Skills';
 import { Works } from './layout/section/works/Works';
 import { Testimony } from './layout/section/testimony/Testimony';
+import { Contact } from './layout/section/contacts/Contacts';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
             <Skills/>
             <Works />
             <Testimony />
+            <Contact />
         </div>
     );
 }
