502000fa53640fb5da67e37d56e17e16649b9f76
FilippSM_laptop <philippsm666@gmail.com>
2024-10-23 23:35:25 +0300

    add adaptiv for main

--- src/layout/section/main/Main.tsx
+++ src/layout/section/main/Main.tsx
@@ -3,12 +3,13 @@ import photo from "./../../../assets/images/Photo.jpg"
 import { FlexWrapper } from "../../../components/FlexWrapper";
 import { Container } from "../../../components/Container";
 import { theme } from "../../../styles/Theme";
+import { font } from "../../../styles/Common";
 
 export const Main = () => {
     return (
         <StyledMain>
             <Container>
-                <FlexWrapper align={"center"} justify={"space-between"}>
+                <FlexWrapper align={"center"} justify={"space-around"} wrap={"wrap"}> {/* фото и текст при уменьшении экрана центрируются  было space-between*/}
                     <div>
                         <SmallText>Hi There</SmallText>
                         <Name>I'm <span>Filipp Samsonau</span></Name>
@@ -32,6 +33,7 @@ const StyledMain = styled.section`
 const PhotoWrapper = styled.div`
     position: relative;
     z-index: 0;
+    margin-top: 65px;
 
     &::before {
         content: "";
@@ -39,10 +41,18 @@ const PhotoWrapper = styled.div`
         height: 470px;
         border: 5px solid ${theme.colors.accent};
 
+       /*  смещение рамки */
         position: absolute;
         top: -24px;
         left: 24px;
         z-index: -1;
+
+        @media ${theme.media.mobile} {
+        width: 314px;
+        height: 414px;
+        top: -17px;
+        left: 20px;
+        }
     }
 `
 
@@ -50,16 +60,23 @@ const Photo = styled.img`
     width: 350px;
     height: 430px;
     object-fit: cover;
+
+    @media ${theme.media.mobile} {
+        width: 310px;
+        height: 380px;
+    }
 `
 
 const MainTitle = styled.h1`
-    font-weight: 400;
-    font-size: 27px;
+    ${font({weight: 400, Fmax: 27, Fmin: 20})}; //подключение миксина
+    /*  font-weight: 400;
+    font-size: 27px; */
 `
 const Name = styled.h2`
-    font-family: "Josefin Sans", sans-serif;
+    ${font({family: "'Josefin Sans', sans-serif", weight: 700, Fmax: 50, Fmin: 36})}; //подключение миксина
+/*     font-family: "Josefin Sans", sans-serif;
     font-weight: 700;
-    font-size: 50px;
+    font-size: 50px; */
     letter-spacing: 0.05em;
     margin: 10px 0;
 
@@ -67,6 +84,7 @@ const Name = styled.h2`
     span {
         position: relative;
         z-index: 0;
+        white-space: nowrap; //запрет переноса
 
         &::before {
             content: "";
@@ -78,6 +96,10 @@ const Name = styled.h2`
             position: absolute;
             bottom: 0;
             z-index: -1;
+        }
+
+        @media ${theme.media.mobile} {
+            margin: 15px 0 22px;
         }
     }
 `
