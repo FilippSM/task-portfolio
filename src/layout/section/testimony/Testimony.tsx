71c810b1578bf2a58995327c2e476e860b4ed516
FilippSM_laptop <philippsm666@gmail.com>
2024-10-24 23:13:48 +0300

    add adaptiv testimony

--- src/layout/section/testimony/Testimony.tsx
+++ src/layout/section/testimony/Testimony.tsx
@@ -4,18 +4,21 @@ import { Icon } from "../../../components/icon/Icon";
 import { Slider } from "../../../components/slider/Slider";
 import { FlexWrapper } from "../../../components/FlexWrapper";
 import { IconWrapper } from "../skills/skill/Skill";
+import { Container } from './../../../components/Container';
 
 
 export const Testimony = () => {
     return (
         <StyledTestimony>
-            <SectionTitle>Testimony</SectionTitle>
-            <FlexWrapper direction={"column"} align={"center"}>
-                <IconWrapper>
-                    <Icon iconId={"invertedCommas"} width={"42"} height={"32"} viewBox={"0 0 42 32"}/>
-                </IconWrapper>
-                <Slider />
-            </FlexWrapper>
+            <Container>
+                <SectionTitle>Testimony</SectionTitle>
+                <FlexWrapper direction={"column"} align={"center"}>
+                    <IconWrapper>
+                        <Icon iconId={"invertedCommas"} width={"42"} height={"32"} viewBox={"0 0 42 32"}/>
+                    </IconWrapper>
+                    <Slider />
+                </FlexWrapper>
+            </Container>
         </StyledTestimony>
     );
 };
